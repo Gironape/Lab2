4321ea8a76d735bbacd2395a2ed0948d8b15dccd
Gironape <andreyka_kazakov_03@mail.ru>
2022-12-02 17:15:02 +0300

    done

--- 1/creating_csv.py
+++ 1/creating_csv.py
@@ -2,14 +2,14 @@
 import pandas as pd
 
 
-def write_to_file(input_file: str, file_for_x: str, file_for_y: str) -> None:
+def write_to_file(file: str, X_csv: str, Y_csv: str) -> None:
     """Запуск файла с набором данных
     file_for_x файл для  первого стобца
     file_for_y файл для второго столбца
     """
-    df = pd.read_csv(input_file)
-    df["Day"].to_csv(file_for_x, index=False)
-    df["Exchange rate"].to_csv(file_for_y, index=False)
+    df = pd.read_csv(file)
+    df["Date"].to_csv(X_csv, index=False)
+    df["Course"].to_csv(Y_csv, index=False)
 
 
 if __name__ == "__main__":
